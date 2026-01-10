d8af17b58a93571aeb10dfe659057c2f183b8171
TanmayHingankar <132577795+TanmayHingankar@users.noreply.github.com>
2026-01-10 20:42:29 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -44,8 +44,7 @@ app.add_middleware(
     ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    allow_origins=["*"]
+    allow_headers=["*"]
 )
 
 class QuizRequest(BaseModel):
